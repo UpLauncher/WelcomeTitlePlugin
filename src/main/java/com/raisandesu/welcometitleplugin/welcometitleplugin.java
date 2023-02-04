5d3dc946bb0fc4e0dec3aff269359e3d2ebcbce9
雷さん <admin@uplauncher.net>
2023-02-04 22:19:16 +0900

    Version 1.1 release.

--- src/main/java/com/raisandesu/welcometitleplugin/welcometitleplugin.java
+++ src/main/java/com/raisandesu/welcometitleplugin/welcometitleplugin.java
@@ -1,8 +1,6 @@
 package com.raisandesu.welcometitleplugin;
 
 import java.util.logging.Logger;
-import java.io.IOException;
-
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -11,48 +9,62 @@ import com.raisandesu.welcometitleplugin.commands.*;
 
 import net.md_5.bungee.api.ChatColor;
 
-import java.awt.*;
-import java.awt.image.BufferedImage;
-
 public final class welcometitleplugin extends JavaPlugin implements Listener {
     public final Logger logger = this.getLogger();
     public welcometitleplugin wt;
 
     @Override
     public void onEnable() {
+        var version = getDescription().getVersion();
+        var prefix = ChatColor.GREEN + this.getConfig().getString("messages.prefix");
         this.saveDefaultConfig();
         if (Bukkit.getVersion().contains("Spigot")) {
             logger.info("");
             logger.info(ChatColor.GREEN + " \\ \\      / /_   _|");
-            logger.info(ChatColor.GREEN + "  \\ \\ /\\ / /  | |   " + ChatColor.AQUA + "WT" + ChatColor.GREEN + " version 1.0.0");
-            logger.info(ChatColor.GREEN + "   \\ V  V /   | |   Bukkit - Spigot で実行中");
+            logger.info(ChatColor.GREEN + "  \\ \\ /\\ / /  | |   " + ChatColor.AQUA + "WT" + ChatColor.GREEN + " version " + version);
+            if (this.getConfig().getString("messages.plugin-enabling-info-first") == "") {
+                logger.info(ChatColor.GREEN + "   \\ V  V /   | |   Bukkit - Spigot " + ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("messages.plugin-enabling-info-final")));
+            } else {
+                logger.info(ChatColor.GREEN + "   \\ V  V /   | |  " + ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("messages.plugin-enabling-info-first") + " Bukkit - Spigot"));
+
+            }
             logger.info(ChatColor.GREEN + "    \\_/\\_/    |_|");   
             logger.info("");
-            logger.warning("-------Purpurを使用する-------");
-            logger.warning("Purpur を使用することで、Paper より高いパフォーマンス");
-            logger.warning("と便利で簡単な機能を手に入れることができます。");
-            logger.warning("tpsbar がデフォルトでついていたり、サーバー名を変更できます。");
+            logger.warning(ChatColor.translateAlternateColorCodes('&', prefix + this.getConfig().getString("messages.purpur-title")));
+            logger.warning(ChatColor.translateAlternateColorCodes('&', prefix + this.getConfig().getString("messages.purpur-desc1")));
+            logger.warning(ChatColor.translateAlternateColorCodes('&', prefix + this.getConfig().getString("messages.purpur-desc2")));
+            logger.warning(ChatColor.translateAlternateColorCodes('&', prefix + this.getConfig().getString("messages.purpur-desc3")));
         } else if (Bukkit.getVersion().contains("Paper")) {
             logger.info(ChatColor.GREEN + " \\ \\      / /_   _|");
-            logger.info(ChatColor.GREEN + "  \\ \\ /\\ / /  | |   " + ChatColor.AQUA + "WT" + ChatColor.GREEN + " version 1.0.0");
-            logger.info(ChatColor.GREEN + "   \\ V  V /   | |   Bukkit - Paper で実行中");
+            logger.info(ChatColor.GREEN + "  \\ \\ /\\ / /  | |   " + ChatColor.AQUA + "WT" + ChatColor.GREEN + " version " + version);
+            if (this.getConfig().getString("messages.plugin-enabling-info-first") == "") {
+                logger.info(ChatColor.GREEN + "   \\ V  V /   | |   Bukkit - Paper " + ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("messages.plugin-enabling-info-final")));
+            } else {
+                logger.info(ChatColor.GREEN + "   \\ V  V /   | |  " + ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("messages.plugin-enabling-info-first")) + " Bukkit - Paper");
+
+            }
             logger.info(ChatColor.GREEN + "    \\_/\\_/    |_|");
-            logger.warning("-------Purpurを使用する-------");
-            logger.warning("Purpur を使用することで、Paper より高いパフォーマンス");
-            logger.warning("と便利で簡単な機能を手に入れることができます。");
-            logger.warning("tpsbar がデフォルトでついていたり、サーバー名を変更できます。");
+            logger.warning(ChatColor.translateAlternateColorCodes('&', prefix + this.getConfig().getString("messages.purpur-title")));
+            logger.warning(ChatColor.translateAlternateColorCodes('&', prefix + this.getConfig().getString("messages.purpur-desc1")));
+            logger.warning(ChatColor.translateAlternateColorCodes('&', prefix + this.getConfig().getString("messages.purpur-desc2")));
+            logger.warning(ChatColor.translateAlternateColorCodes('&', prefix + this.getConfig().getString("messages.purpur-desc3")));
         } else if (Bukkit.getVersion().contains("Purpur")) {
             logger.info(ChatColor.GREEN + " \\ \\      / /_   _|");
-            logger.info(ChatColor.GREEN + "  \\ \\ /\\ / /  | |   " + ChatColor.AQUA + "WT" + ChatColor.GREEN + " version 1.0.0");
-            logger.info(ChatColor.GREEN + "   \\ V  V /   | |   Bukkit - Purpur で実行中");
+            logger.info(ChatColor.GREEN + "  \\ \\ /\\ / /  | |   " + ChatColor.AQUA + "WT" + ChatColor.GREEN + " version " + version);
+            if (this.getConfig().getString("messages.plugin-enabling-info-first") == "") {
+                logger.info(ChatColor.GREEN + "   \\ V  V /   | |   Bukkit - Purpur " + ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("messages.plugin-enabling-info-final")));
+            } else {
+                logger.info(ChatColor.GREEN + "   \\ V  V /   | |  " + ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("messages.plugin-enabling-info-first")) + " Bukkit - Purpur");
+
+            }
             logger.info(ChatColor.GREEN + "    \\_/\\_/    |_|");
         } else {
-            logger.warning("-------不明なBukkit-------");
-            logger.warning("この不明な Bukkit では、確認されていないため実行できません。");
-            logger.warning("Purpur をおすすめします: https://purpurmc.org");
+            logger.warning(ChatColor.translateAlternateColorCodes('&', prefix + this.getConfig().getString("messages.unknown-title")));
+            logger.warning(ChatColor.translateAlternateColorCodes('&', prefix + this.getConfig().getString("messages.unknown-desc1")));
+            logger.warning(ChatColor.translateAlternateColorCodes('&', prefix + this.getConfig().getString("messages.unknown-desc2")));
             this.getPluginLoader().disablePlugin(wt);
         }
-        logger.info(ChatColor.AQUA + "WelcomeTitlePlugin " + ChatColor.GREEN + "が有効になりました。");
+        logger.info(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("messages.plugin-enabled")));
         getServer().getPluginManager().registerEvents(new userjoined(this), this);
         getCommand("wtreload").setExecutor(new wtreload(this));
         getCommand("wt").setExecutor(new wtCommand(this));
@@ -60,7 +72,7 @@ public final class welcometitleplugin extends JavaPlugin implements Listener {
 
     @Override
     public void onDisable() {
-        logger.info("WelcomeTitlePlugin が無効になりました。");
+        logger.info(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("messages.plugin-disabled")));
         Bukkit.getScheduler().cancelTasks(this);
     }
 
